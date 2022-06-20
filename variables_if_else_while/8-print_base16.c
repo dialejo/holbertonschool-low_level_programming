3468b2b7c51fd6080f960ef932c0b746a4f5632c
dialejo <diegomaldonadom1992@gmail.com>
2022-06-19 19:45:24 -0700

    base 16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - prints base 16 numbers.
+(*
+ * Return: cero
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	i = 'a';
+	while (i <= 'f')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+
+}
