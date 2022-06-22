8d51fba09ebcb99c3eecc98ddb2676912b9adb28
dialejo <diegomaldonadom1992@gmail.com>
2022-06-22 16:47:53 -0700

    string

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,20 +1,20 @@
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
+
 /**
  * print_rev - prints a string, followed by a new line.
  * @s: input string to print.
  */
 void print_rev(char *s)
 {
-	int i;
+	int i, len,temp;
+	len = strlen(s);
 
-	while (s[i] != '\0')
+	for (i = 0; i < len/2; i++)
 	{
-		i++;
+		temp = s[i];
+		s[i] = str1[len - i - 1];
+		str1[len -i -1] = temp;
 	}
-	i--;
-	while (s[i] != '\0')
-	{
-		_putchar(s[i--]);
-	}
-	_putchar('\n');
 }
