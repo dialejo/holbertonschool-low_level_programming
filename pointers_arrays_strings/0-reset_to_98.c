2870093e6a2754ba15467756e3d8c9c9433e582c
dialejo <diegomaldonadom1992@gmail.com>
2022-06-22 09:54:48 -0700

    pointers

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - pointer to an int as parameter and updates.
+ * @n: value.
+ * return: cero
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
