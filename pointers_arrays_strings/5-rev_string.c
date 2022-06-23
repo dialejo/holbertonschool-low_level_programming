6d227d2e2b7a535aacd6058504732ac71c741b1f
dialejo <diegomaldonadom1992@gmail.com>
2022-06-22 20:16:07 -0700

    back and for

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -13,9 +13,13 @@ void rev_string(char *s)
 int i, x;
 int t = 0;
 for (i = 0 ; s[i] != '\0' ; i++)
+{
 t++;
 _putchar(s[i]);
+}
 for (x = t - 1 ; x >= 0 ; x--)
+{
 _putchar(s[x]);
 _putchar('\n');
+}
 }
