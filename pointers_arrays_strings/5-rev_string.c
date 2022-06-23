9602cf001eb5d58caa9b742ea8726cf3e763985a
dialejo <diegomaldonadom1992@gmail.com>
2022-06-22 19:39:03 -0700

    back and front

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - array to display the array
+ *
+ * @s: string
+ *
+ */
+
+void rev_string(char *s)
+{
+int i, x;
+int t = 0;
+{
+for (i = 0 ; s[i] != '\0' ; i++)
+t++;
+_putchar(s[i]);
+}
+for (x = (t - 1) ; x >= 0 ; x--)
+_putchar(s[x]);
+}
