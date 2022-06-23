cace2695f913eb9666e1d2e3b1dd3e73cad8dcc2
dialejo <diegomaldonadom1992@gmail.com>
2022-06-23 08:43:37 -0700

    print half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts_half - display half of the spring
+ *
+ * @str: string
+ *
+ *
+ */
+
+void puts_half(char *str)
+{
+int len, x, y;
+len = strlen(str);
+
+if (len % 2 == 0)
+	for (x = len + 1 ; ststr[x] != '\0' ; x++)
+		_putchar(str[x]);
+else
+	for (y = len ; str[y] != '\0' ; y++)
+		_putchar(str[y]);
+_putchar('\n');
+}
