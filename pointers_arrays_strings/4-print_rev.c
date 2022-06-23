43dd972a0258bab50e55b7cfeab8452b2eb27314
dialejo <diegomaldonadom1992@gmail.com>
2022-06-22 18:22:40 -0700

    for runner

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -8,16 +8,10 @@
  */
 void print_rev(char *s)
 {
-	int i;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	i--;
-	while (s[i] != '\0')
-	{
-		_putchar(s[i--]);
-	}
-	_putchar('\n');
+int x, y;
+int z = 0;
+for (y = 0 ; s[y] != '/0' ; y++)
+z++;
+for (x = t - 1 ; x >= 0 ; x--)
+_putchar(s[x]);	
 }
