b0fe81c9861eb23329743ad8178b42fb9f2444ab
dialejo <diegomaldonadom1992@gmail.com>
2022-06-22 19:44:34 -0700

    back and forward

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -12,12 +12,14 @@ void rev_string(char *s)
 {
 int i, x;
 int t = 0;
-{
+
 for (i = 0 ; s[i] != '\0' ; i++)
+{
 t++;
 _putchar(s[i]);
-_putchar('\n');
 }
 for (x = (t - 1) ; x >= 0 ; x--)
+{
 _putchar(s[x]);
+}
 }
