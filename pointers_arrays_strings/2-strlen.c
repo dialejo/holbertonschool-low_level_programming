cb5af945c7712d1bbdad5c0b249d1ebdc1f0c02c
dialejo <diegomaldonadom1992@gmail.com>
2022-06-22 18:39:54 -0700

    print the len

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * int _strlen - print the len of the array
+ *
+ *@s : string
+ *
+ * Return the len of the array
+ */
+
+void int _strlen(char *s)
+{
+int i;
+int z = 0;
+for (i = 0 ; s[i] != '\0' ; i++)
+{
+z++;
+}
+_putchar(z);
