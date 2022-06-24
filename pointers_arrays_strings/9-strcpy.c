2181e45b201f0fb1242976e4b7cdb69d7076594d
dialejo <diegomaldonadom1992@gmail.com>
2022-06-23 17:59:42 -0700

    strcpy

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -8,15 +8,13 @@
  * @dest: pointer
  * @src: string
  *
- * return: cero
+ * return: dest
  */
 
 char *_strcpy(char *dest, char *src)
 {
 int i;
 for (i = 0 ; src[i] <= '\0' ; i++)
-{
 dest[i] = (src[i]);
 return (dest);
-}
 }
