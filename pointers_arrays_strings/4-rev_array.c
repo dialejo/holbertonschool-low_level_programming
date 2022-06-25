39a69baf630f1fad2e589981caa966ff98026784
dialejo <diegomaldonadom1992@gmail.com>
2022-06-25 09:17:11 -0700

    rev-array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - display a array in reverse
+ * @a: string
+ * @n: number of strings
+ *
+ */
+void reverse_array(int *a, int n)
+{
+int x;
+int i = 0;
+while (i < n)
+	i++;
+for (x = i - 1; a[x] >= 0 ;  x--)
+{
+}
+}
