59fe47d5a556f5e9c1ee9e5e8d4bc62ac1fe9c7c
dialejo <diegomaldonadom1992@gmail.com>
2022-06-24 18:14:06 -0700

    strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -5,22 +5,21 @@
  * _strcmp - compare a string
  * @s1: string
  * @s2: string
- * Return: cero;
+ * Return: m
  */
 int _strcmp(char *s1, char *s2)
 {
 int i = 0;
 int y = 0;
-int x, t, z;
-
+int x, t, m;
 for (t = 0 ; s1[t] != '\0' ; t++)
 	i++;
 for (x = 0 ; s2[x] != '\0' ; x++)
 	y++;
 if (i < y)
-	z = (-15);
+	m = (-15);
 else if (i > y)
-	z = (15);
-return (0);
+	m = (15);
+return (m);
 }
 
