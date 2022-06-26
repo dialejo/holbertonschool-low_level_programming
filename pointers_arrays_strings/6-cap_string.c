b4ccdd0d0a5bde088a657ee3fe022614bbb07f28
dialejo <diegomaldonadom1992@gmail.com>
2022-06-26 16:50:23 -0700

    cap string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,24 +2,24 @@
 #include <stdio.h>
 /**
  * *string_toupper - string to upper case
- * @s: string
+ * @*: string
  * Return: cero
  */
 
-char *string_toupper(char *s)
+char *string_toupper(char *)
 {
 int i, j;
 char separadores[] = " \t\n,;.!?\"(){}";
 i = 1;
 
-if (s[0] >= 'a' && s[0] <= 'z')
-	s[0] -= ('a' - 'A');
+if (*[0] >= 'a' && *[0] <= 'z')
+	*[0] -= ('a' - 'A');
 
-while (s[i] != '\0')
+while (*[i] != '\0')
 {
 	for (j = 0; separadores[j] != '\0'; j++)
-		if (s[i - 1] == separadores[j] && (s[i] >= 'a' && s[i] <= 'z'))
-			s[i] -= ('a' - 'A');
+		if (*[i - 1] == separadores[j] && (*[i] >= 'a' && *[i] <= 'z'))
+			*[i] -= ('a' - 'A');
 	i++;
 }
 return (s);
