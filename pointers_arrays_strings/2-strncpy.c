34543068bcbe439a8df3a7c47afb5aa21fca260c
dialejo <diegomaldonadom1992@gmail.com>
2022-06-26 17:34:03 -0700

    strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,15 +11,10 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int i = 0;
-while (i < n && src[i] != '\0')
-{
-	dest[i] = src[i];
-	i++;
-}
-{
-if (src[i] == '\0')
-	dest[i] = '\0';
-}
+for (i = 0; src[i] != '\0' && i < n; i++)
+	dest[i]	= src[i];
+while (i < n)
+	dest[i++] = '\0';
 return (dest);
 }
 
