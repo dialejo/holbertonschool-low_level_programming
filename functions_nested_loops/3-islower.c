7505dc6b3726ee82315d3eddf327da0ccae61853
dialejo <diegomaldonadom1992@gmail.com>
2022-06-26 18:19:38 -0700

    islow

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - print if it's lower
+ * @c = string
+ *Return: 1 or 0
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+	return (1);
+else
+	return (0);
+}
