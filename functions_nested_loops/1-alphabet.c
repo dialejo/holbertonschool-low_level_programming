280479782b4405542e8f96f61efbf4156104eb2f
dialejo <diegomaldonadom1992@gmail.com>
2022-06-26 18:05:37 -0700

    alphabets

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabet in lower cases
+ *
+ *
+ */
+
+void print_alphabet(void)
+{
+char s;
+for (s = 'a'; s <= 'z'; s++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
